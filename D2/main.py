a84d6645a3681aa3e190ce07e80205116a8776b2
Gritier <Gritier@github.com>
2024-12-04 18:25:22 +0100

    D2 P1 + P2 in progress

--- D2/main.py
+++ D2/main.py
@@ -3,11 +3,11 @@ def get_input(file="input.txt"):
         return f.read().splitlines()
 
 def verify_report(report):
-    if not (all(int(report[i]) <= int(report[i + 1]) for i in range(len(report) - 1)) or all(int(report[i]) >= int(report[i + 1]) for i in range(len(report) - 1))):
+    if not (all(report[i] <= report[i + 1] for i in range(len(report) - 1)) or all(report[i] >= report[i + 1] for i in range(len(report) - 1))):
         return 0
     else:
         for i in range(len(report) - 1):
-            diff = int(report[i]) - int(report[i + 1])
+            diff = report[i] - report[i + 1]
             if  diff not in [3,2,1,-1,-2,-3]:
                 return 0
     return 1
@@ -17,19 +17,22 @@ def dampener(report):
     errors = []
     for idx, el in enumerate(report):
         if idx != 0:
-            diff = int(el) - int(report[idx-1])
+            diff = el - report[idx-1]
             if diff < 0 or diff < -3:
                 if direction > 0:
                     errors.append(idx)
+                    errors.append(idx-1)
                 direction = -1
             elif diff > 0 or diff > 3:
                 if direction < 0:
                     errors.append(idx)
+                    errors.append(idx-1)
                 direction = 1
             else:
                 errors.append(idx)
+                errors.append(idx-1)
                 direction = 0
-        if len(errors) > 2:
+        if len(errors) > 4:
             return 0
     for error in errors:
         temp = list(report)
@@ -42,11 +45,28 @@ def main():
     input = get_input()
     safe_reports = 0
     for line in input:
-        if verify_report(line.split(" ")):
+        report = [int(value) for value in line.split(" ")]
+        if verify_report(report):
             safe_reports += 1
-        elif dampener(line.split(" ")):
+        elif dampener(report):
             safe_reports += 1
     print(safe_reports)
 
 main()
+
+def test():
+    input = get_input()
+    safe = 0
+
+    for line in input:
+        errors = []
+        report = [int(value) for value in line.split(" ")]
+        for i in range(len(report)-1):
+            diff = report[i] - report[i+1]
+            if diff not in [-3, -2, -1, 1, 2, 3]:
+                error.append((i,i+1))
+
+
+#test()
+
 
