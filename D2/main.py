68fa52f34e53788a50defe214daa0845665f8d0c
Gritier <28902214+Gritier@users.noreply.github.com>
2024-12-05 01:33:20 +0100

    Full D2

--- D2/main.py
+++ D2/main.py
@@ -12,31 +12,10 @@ def verify_report(report):
                 return 0
     return 1
 
-def dampener(report):
-    direction = 0
-    errors = []
-    for idx, el in enumerate(report):
-        if idx != 0:
-            diff = el - report[idx-1]
-            if diff < 0 or diff < -3:
-                if direction > 0:
-                    errors.append(idx)
-                    errors.append(idx-1)
-                direction = -1
-            elif diff > 0 or diff > 3:
-                if direction < 0:
-                    errors.append(idx)
-                    errors.append(idx-1)
-                direction = 1
-            else:
-                errors.append(idx)
-                errors.append(idx-1)
-                direction = 0
-        if len(errors) > 4:
-            return 0
-    for error in errors:
+def full_damp(report):
+    for idx,el in enumerate(report):
         temp = list(report)
-        temp.pop(error)
+        temp.pop(idx)
         if verify_report(temp):
             return 1
     return 0
@@ -48,25 +27,10 @@ def main():
         report = [int(value) for value in line.split(" ")]
         if verify_report(report):
             safe_reports += 1
-        elif dampener(report):
+        elif full_damp(report):
             safe_reports += 1
     print(safe_reports)
 
 main()
-
-def test():
-    input = get_input()
-    safe = 0
-
-    for line in input:
-        errors = []
-        report = [int(value) for value in line.split(" ")]
-        for i in range(len(report)-1):
-            diff = report[i] - report[i+1]
-            if diff not in [-3, -2, -1, 1, 2, 3]:
-                error.append((i,i+1))
-
-
-#test()
 
 
