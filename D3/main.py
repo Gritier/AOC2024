0b90dfdc43f6c6a370af3753facae9d75b11e9da
Gritier <28902214+Gritier@users.noreply.github.com>
2024-12-11 18:26:12 +0100

    Full D3

--- D3/main.py
+++ D3/main.py
@@ -9,12 +9,37 @@ def analyze_memory(memory):
 
 def advanced_analysis(memory):
     pattern = r"(do\(\)|don't\(\))|mul\(\d{1,3},\d{1,3}\)"
-    return re.findall(pattern, memory)
+    return re.finditer(pattern, memory)
 
 def multiply(mul):
     factors = [int(number) for number in mul.replace("mul(","").replace(")","").split(",")]
     return factors[0]*factors[1]
- 
+
+def part_two():
+    input = get_input("input.txt")
+    total = 0
+    context = "do"
+    for memory in input:
+        matches = advanced_analysis(memory)
+        category = {
+            "do":[],
+            "don't":[]
+        }
+
+        for match in matches:
+            value = match.group()
+            if "do()" in value:
+                context = "do"
+            elif "don't()" in value:
+                context = "don't"
+            else:
+                category[context].append(value)
+
+        for mul in category["do"]:
+            total += multiply(mul)
+
+    print(total)
+
 def main():
     input = get_input()
     total = 0
@@ -23,33 +48,6 @@ def main():
         for m in mul:
             total += multiply(m)
     print(total)
+    part_two()
 
-#main()
-
-def test():
-    input = get_input("example.txt")
-    total = 0
-    for memory in input:
-        matches = advanced_analysis(memory)
-        print(matches)
-        
-        context = "do"
-        category = {
-            "do":[],
-            "don't":[]
-        }
-
-        for match in matches:
-            print(match)
-            if match[0] and ("do" in match[0] or "don't" in match[0]):
-                context = "do" if "do" in match[0] else "don't"
-            elif match[1]:
-                category[context].append(match[1])
-        
-        for mul in category["do"]:
-            total += multiply(mul)
-    print(total)
-
-
-
-test()
+main()
